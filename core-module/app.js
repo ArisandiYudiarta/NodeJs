5affd63ba8b07393b60d804eb7b27861303753c2
Arisandi Yudiarta <arisandiyudiarta21@gmail.com>
2022-02-24 11:49:38 +0800

    core modules

---
+++ core-module/app.js
@@ -0,0 +1,56 @@
+// Core Module==================
+
+// File System 
+
+const fs = require('fs');
+
+// // menuliskan string ke file secara synchronous
+// try{
+//     fs.writeFileSync('data/test.txt', 'Menuliskan file secara synchronous!');
+// }catch(e){
+//     console.log(e)
+// }
+
+// menuliskan string ke file secara asnychronous 
+
+// fs.writeFile('data/test.txt', 'Menulis file secara asynchronous', (e) => {
+//     console.log(e);
+// });
+
+// membaca isi file (synchronous) 
+
+// const data = fs.readFileSync('data/test.txt', 'utf-8');
+// console.log(data);
+
+// membaca isi file secara asynchronous 
+
+// fs.readFile('data/test.txt', 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+
+// Readline 
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout 
+});
+
+rl.question('Masukkan nama anda : ', (nama) =>{
+    rl.question('Selanjutnya silahkan masukkan umur anda : ', (noHp) => {
+        const contact = { nama, noHp };
+        const file = fs.readFileSync('data/contact.json', 'utf-8');
+        const contacts = JSON.parse(file);
+        
+        contacts.push(contact);
+        console.log(contacts);
+
+        fs.writeFileSync('data/contact.json', JSON.stringify(contacts));
+        console.log('sukses');
+
+        rl.close();
+    });
+});
